21c755b8747cc20b42ed20ed4b05cf0bdbc57c61
Allaaxx <allanrao358@gmail.com>
2026-09-08 18:25:31 -0300

    feat: add login page with form validation

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,5 +1,119 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Controller, useForm } from 'react-hook-form';
+import { Link } from 'react-router';
+import z from 'zod';
+
+import PasswordInput from '@/components/password-input';
+import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field';
+import { Input } from '@/components/ui/input';
+
 const SignInPage = () => {
-  return <h1>Sign In Page</h1>;
+  const signInSchema = z.object({
+    email: z
+      .email({
+        error: 'O e-mail é inválido.',
+      })
+      .trim()
+      .min(1, {
+        error: 'O e-mail é obrigatório.',
+      }),
+    password: z.string().trim().min(6, {
+      error: 'A senha deve ter no minímo 6 caracteres.',
+    }),
+  });
+
+  const form = useForm({
+    resolver: zodResolver(signInSchema),
+    mode: 'onSubmit',
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  const handleSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div className="flex h-screen w-screen flex-col items-center justify-center gap-3">
+      <Card className="w-full max-w-lg">
+        <CardHeader>
+          <CardTitle>Entre na sua conta</CardTitle>
+          <CardDescription>Insira seus dados abaixo.</CardDescription>
+        </CardHeader>
+        <CardContent className="space-y-4">
+          <form id="form-sign-in" onSubmit={form.handleSubmit(handleSubmit)}>
+            <FieldGroup>
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor={field.name}>E-mail</FieldLabel>
+                    <Input
+                      {...field}
+                      id="email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Digite seu email"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel htmlFor={field.name}>Senha</FieldLabel>
+                    <PasswordInput
+                      {...field}
+                      id="password"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Digite sua senha"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
+        </CardContent>
+        <CardFooter>
+          <Button className="w-full" type="submit" form="form-sign-in">
+            Fazer login
+          </Button>
+        </CardFooter>
+      </Card>
+      <div className="flex items-center justify-center">
+        <p className="text-center opacity-50">Ainda não possui uma conta? </p>
+        <Button variant="link">
+          <Link to="/signup">Crie agora</Link>
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default SignInPage;

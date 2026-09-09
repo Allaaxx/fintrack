86e4c3f4171126d7ec7c92ed287d35e8be0cfe15
Allaaxx <allanrao358@gmail.com>
2026-09-09 08:35:27 -0300

    feat: ensure local storage are used to reauthenticate user on signup page

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,6 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import { useMutation } from '@tanstack/react-query';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 import { Link } from 'react-router';
 import z from 'zod';
@@ -84,6 +84,27 @@ const SignUpPage = () => {
       terms: false,
     },
   });
+
+  useEffect(() => {
+    const init = async () => {
+      try {
+        const accessToken = localStorage.getItem('accessToken');
+        const refreshToken = localStorage.getItem('refreshToken');
+        if (!accessToken && !refreshToken) return;
+        const response = await api.get('/users/me', {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+        setUser(response.data);
+      } catch (error) {
+        localStorage.removeItem('accessToken');
+        localStorage.removeItem('refreshToken');
+        console.error(error);
+      }
+    };
+    init();
+  }, []);
 
   const handleSubmit = (data) => {
     signUpMutation.mutate(data, {

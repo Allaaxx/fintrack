af651446370f18db20ff13fa6f001172c576f4e0
Allaaxx <allanrao358@gmail.com>
2026-09-11 18:57:49 -0300

    fix: correct typo in transactions table header

--- src/components/transactions-table.jsx
+++ src/components/transactions-table.jsx
@@ -35,7 +35,7 @@ const columns = [
           variant="ghost"
           onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
         >
-          Tipof
+          Tipo
           <ArrowUpDown className="ml-2 h-4 w-4" />
         </Button>
       );
